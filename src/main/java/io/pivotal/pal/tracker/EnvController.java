82bd8b75c89cc91e35211ce796a1401af2a19376
Christopher Klewes <christopher.klewes@gmail.com>
2018-12-17 15:37:25 +0000

    Deploy a Spring Boot app to Cloud Foundry

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,31 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private final Map<String, String> env = new HashMap<>();
+
+    public EnvController(
+            @Value("${PORT:NOT SET}") String port,
+            @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit,
+            @Value("${CF_INSTANCE_INDEX:NOT SET}") String instanceIndex,
+            @Value("${CF_INSTANCE_ADDR:NOT SET}") String instanceAddress
+    ) {
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memoryLimit);
+        env.put("CF_INSTANCE_INDEX", instanceIndex);
+        env.put("CF_INSTANCE_ADDR", instanceAddress);
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        return env;
+    }
+}
